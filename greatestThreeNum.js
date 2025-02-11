2ec97e4ca2bcc1f7c2f69577ecf614350a5a8c4f
Renuka Devi <renuka@xminds.com>
2025-02-11 14:22:22 +0530

    Greatest among three numbers

--- greatestThreeNum.js
+++ greatestThreeNum.js
@@ -3,12 +3,8 @@ const num1 = parseFloat(prompt("Enter first number: "));
 const num2 = parseFloat(prompt("Enter second number: "));
 const num3 = parseFloat(prompt("Enter third number: "));
 
-if(num1 > num2) {
-    if(num1 > num3) {
-        console.log(`number ${num1} is greatest number`);
-    } else {
-        console.log(`number ${num3} is greatest number`);
-    }
+if((num1 > num2) && (num1 > num3)) {
+    console.log(`number ${num1} is greatest number`);
 } else if(num2 > num3) {
     console.log(`number ${num2} is greatest number`);
 } else {
