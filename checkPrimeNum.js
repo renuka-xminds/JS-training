2c44872a7c2523c1be964aaa819507fb46af0d7b
Renuka Devi <renuka@xminds.com>
2025-02-11 16:26:06 +0530

    Check given number is prime or not

---
+++ checkPrimeNum.js
@@ -0,0 +1,14 @@
+const prompt=require('prompt-sync')();
+let num = parseFloat(prompt("Enter number: "));
+
+if(num != 1) {
+    let i = 2;
+    while(i < num-1) {
+        if(num % i == 0) {
+            console.log(`${num} is not a prime number`);
+            break;
+        }
+        i++;
+    }
+    console.log(`${num} is a prime number`);
+}
