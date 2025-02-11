9feec8e267185d79a25e8d7727ac084178c3de6c
Renuka Devi <renuka@xminds.com>
2025-02-11 13:58:15 +0530

    JS

---
+++ greatestThreeNum.js
@@ -0,0 +1,18 @@
+const prompt=require('prompt-sync')();
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+
+if(num1 > num2) {
+    if(num1 > num3) {
+        console.log(`number ${num1} is greatest number`);
+    } else {
+        console.log(`number ${num3} is greatest number`);
+    }
+} else if(num2 > num3) {
+    console.log(`number ${num2} is greatest number`);
+} else {
+    console.log(`number ${num3} is greatest number`);
+}
+
+
---
+++ helloworld.js
@@ -0,0 +1,18 @@
+console.log("HELLO WORLD");
+// Double quoted string
+let nameq = "Renuka";
+// backticks string
+let des = `${nameq} is a WEB DEVELOPER`;
+//single quoted string
+let z = 'Perfect destination';
+console.log(des);
+console.log(z);
+console.log(nameq[0]);
+console.log(nameq.charAt(1));   
+let x1= 5;
+//console.log(x1++);
+//console.log(x1);
+
+for(var i =1; i<=2; ++i){
+    console.log(x1++);
+}
